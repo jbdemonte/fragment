9df318d5a739691b8acb2b855185824594a9dc88
Jean-Baptiste Demonte <jbdemonte@gmail.com>
2012-12-12 22:06:41 +0100

    optimize by avoiding the data access

--- fragment.js
+++ fragment.js
@@ -1,46 +1,77 @@
 (function ($) {
-    var namespace = "fragment";
-
-    function getFragment(element) {
-        var $e = $(element),
-            fragment = $e.data(namespace);
-        if (!fragment) {
-            fragment = $(document.createDocumentFragment());
-            $e.data(namespace, fragment);
+    var fragments = [];
+    
+    function index(element) {
+        for (var i = 0; i < fragments.length; i++) {
+            if (fragments[i].element === element) {
+                return i;
+            }
         }
-        return fragment;
+        return false;
+    }
+
+    function get(element) {
+        var i = index(element);
+        if (i === false) {
+          fragments.push({element: element, fragment: $(document.createDocumentFragment())});
+          i = fragments.length - 1;
+        }
+        return fragments[i];
+    }
+    
+    function remove(index) {
+        fragments[index].fragment = null;
+        fragments[index].element = null;
+        delete fragments[index];
+        for (var i = 0; i < fragments.length; i++) {
+            if (fragments[i]) {
+                return;
+            }
+        }
+        fragments = [];
     }
 
     $.fn.appendToFragment = function () {
         var args = arguments;
-        return this ? this.each(function (_, element) {
-            $.fn.append.apply(getFragment(element), args);
-        }) : this;
+        return this.each(function (_, element) {
+            $.fn.append.apply(get(element).fragment, args);
+        });
     };
 
     $.fn.prependToFragment = function () {
         var args = arguments;
-        return this ? this.each(function (_, element) {
-            $.fn.prepend.apply(getFragment(element), args);
-        }) : this;
+        return this.each(function (_, element) {
+            $.fn.prepend.apply(get(element).fragment, args);
+        });
     };
 
     $.fn.appendFragment = function () {
-        return this ? this.each(function (_, element) {
-            var $e = $(element);
-            $e.append($e.data(namespace)).removeData(namespace);
-        }) : this;
+        return this.each(function (_, element) {
+            var i = index(element);
+            if (i !== false) {
+                $(element).append(fragments[i].fragment);
+                remove(i);
+            }
+        });
     };
 
     $.fn.prependFragment = function () {
-        return this ? this.each(function (_, element) {
-            var $e = $(element);
-            $e.prepend($e.data(namespace)).removeData(namespace);
-        }) : this;
+        return this.each(function (_, element) {
+            var i = index(element);
+            if (i !== false) {
+                $(element).prepend(fragments[i].fragment);
+                remove(i);
+            }
+        });
     };
 
     $.fn.removeFragment = function () {
-        return this ? this.removeData(namespace) : this;
+        return this.each(function (_, element) {
+            var i = index(element);
+            if (i !== false) {
+                remove(i);
+            }
+        });
     };
 
 })(jQuery);
